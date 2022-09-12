84cc40c5a988102fb5f22606f4eecbb2144a1d21
Volker Simonis <volker.simonis@gmail.com>
2022-09-12 22:05:11 +0200

    Add options for snapshotting, restoring and killing to fireCRaCer.sh

--- fireCRaCer.sh
+++ fireCRaCer.sh
@@ -15,7 +15,8 @@ MYPATH=$(dirname $(realpath -s $0))
 # sudo ip addr add 172.16.0.1/24 dev tap1
 # sudo ip link set tap1 up
 
-while getopts 'lmni:h?' opt; do
+
+while getopts 'lmns:r:i:ukh?' opt; do
   case "$opt" in
     l)
       LOGGING=1
@@ -23,20 +24,37 @@ while getopts 'lmni:h?' opt; do
     m)
       METRICS=1
       ;;
+    s)
+      SNAPSHOT="$OPTARG"
+      ;;
+    r)
+      RESTORE="$OPTARG"
+      ;;
+    u)
+      USERFAULTFD=1
+      ;;
     n)
       NO_SERIAL="8250.nr_uarts=0"
       ;;
     i)
       RW_IMAGE="$OPTARG"
       ;;
+    k)
+      KILL=1
+      ;;
     ?|h)
-      echo "Usage: $(basename $0) [-i <rw-image>] [-l] [-m] [-n]"
+      echo "Usage: $(basename $0) [[-i <rw-image>] [-l] [-m] [-n]] [-s <snapshot-dir>] [-r <snapshot-dir> [-l] [-u]]"
       echo " -i <rw-image>: a file or device which will be used as read/write overlay for the root file system."
       echo "                By default a ram disk of TEMPFS_SIZE will be used."
       echo " -l: enable Firecracker logging to LOG_PATH with LOG_LEVEL and LOG_SHOW_LEVEL/LOG_SHOW_ORIGIN"
       echo " -m: enable Firecracker metrics to METRICS_PATH"
       echo " -n: Disable serial devices (i.e 8250.nr_uarts=0)."
       echo "     This saves ~100ms boot time but will disable the boot console."
+      echo ""
+      echo " -s: snapshot Firecracker on tap device ${TAP_DEVICE:-'tap0'} to <snapshot-dir>."
+      echo "     If <snapshot-dir> doesn't exist, it will be created."
+      echo " -r: restore Firecracker snapshot on tap device ${TAP_DEVICE:-'tap0'} from <snapshot-dir>."
+      echo " -k: send Firecracker guest on tap device ${TAP_DEVICE:-'tap0'} CtrlAltDel message (i.e. shut it down)."
       exit 1
       ;;
   esac
@@ -53,9 +71,10 @@ KERNEL=${KERNEL:-"$MYPATH/deps/vmlinux"}
 IMAGE=${IMAGE:-"$MYPATH/deps/rootfs.ext4"}
 
 TAP_DEVICE=${TAP_DEVICE:-'tap0'}
-TAP_DEVICE_NR=`echo $TAP_DEVICE | grep -oP '[0-9]+'`
-if [[ x$TAP_DEVICE_NR = "x" ]]; then
-  echo "TAP_DEVICE must and in a number between 0-255 (was '$TAP_DEVICE')"
+
+TAP_DEVICE_NR=`echo $TAP_DEVICE | grep -oP '^tap[0-9]{1,3}$' | grep -oP '[0-9]{1,3}$'`
+if [[ "x$TAP_DEVICE_NR" == "x" ]]; then
+  echo "TAP_DEVICE must have the form 'tap<num>' with <num> = 0..255 (was '$TAP_DEVICE')"
   exit 1
 fi
 TAP_DEVICE_NR_HEX=$( printf "%.2x" $TAP_DEVICE_NR )
@@ -77,8 +96,75 @@ IP_SETTINGS=`ifconfig $TAP_DEVICE |
     netmask=$4
     printf("%s.%s.%s.%s::%s:%s::eth0:off\n", ip[1], ip[2], ip[3], ip[4] + 1, gateway, netmask)
   }'`
-echo $IP_SETTINGS
+# echo $IP_SETTINGS
 FC_SOCKET=${FC_SOCKET:-"/tmp/fireCRaCer-$TAP_DEVICE.socket"}
+
+check_http_response() {
+  actual="$1"
+  expected="$2"
+  message="$3"
+  if [[ "x$actual" == "x$expected" ]]; then
+    echo "$message: OK"
+  else
+    echo "$message: Error (HTTTP response was $ret)"
+    echo "Exiting.."
+    exit 1
+  fi
+}
+
+if [[ -v KILL ]]; then
+  echo "Killing firecracker instance running on tap device $TAP_DEVICE"
+  ret=$(curl --write-out '%{http_code}' \
+             --silent \
+             --output /dev/null \
+             --unix-socket $FC_SOCKET \
+             -X PUT 'http://localhost/actions' \
+             -H  'Accept: application/json' \
+             -H  'Content-Type: application/json' \
+             -d '{ "action_type": "SendCtrlAltDel" }')
+  check_http_response $ret  "204" "Kill"
+  exit 0
+fi
+
+if [[ -v SNAPSHOT ]]; then
+  if [ ! -d "$SNAPSHOT" ]; then
+    mkdir -p $SNAPSHOT
+  fi
+  # Suspend
+  ret=$(curl --write-out '%{http_code}' \
+             --silent \
+             --output /dev/null \
+             --unix-socket $FC_SOCKET \
+             -X PATCH 'http://localhost/vm' \
+             -H 'Accept: application/json' \
+             -H 'Content-Type: application/json' \
+             -d '{ "state": "Paused" }')
+  check_http_response $ret  "204" "Suspend"
+  # Checkpoint
+  ret=$(curl --write-out '%{http_code}' \
+             --silent \
+             --output /dev/null \
+             --unix-socket $FC_SOCKET \
+             -X PUT 'http://localhost/snapshot/create' \
+             -H 'Accept: application/json' \
+             -H 'Content-Type: application/json' \
+             -d "{ \"snapshot_type\": \"Full\", \
+                   \"snapshot_path\": \"$SNAPSHOT/snapshot_file\", \
+                   \"mem_file_path\": \"$SNAPSHOT/mem_file\", \
+                   \"version\": \"1.0.0\" }")
+  check_http_response $ret "204" "Snapshot"
+  # Resume
+  ret=$(curl --write-out '%{http_code}' \
+             --silent \
+             --output /dev/null \
+             --unix-socket $FC_SOCKET \
+             -X PATCH 'http://localhost/vm' \
+             -H 'Accept: application/json' \
+             -H 'Content-Type: application/json' \
+             -d '{ "state": "Resumed" }')
+  check_http_response $ret  "204" "Resumed"
+  exit 0
+fi
 
 if [[ -v LOGGING ]]; then
   LOG_PATH=${LOG_PATH:-"/tmp/fireCRaCer-$TAP_DEVICE.log"}
@@ -87,7 +173,16 @@ if [[ -v LOGGING ]]; then
   LOG_SHOW_ORIGIN=${LOG_SHOW_ORIGIN:-"true"}
   # The logs file has to be empty!
   truncate -s 0 $LOG_PATH
-  LOGGER=$(cat <<EOF
+  if [[ -v RESTORE ]]; then
+    LOGGER="--log-path $LOG_PATH --level $LOG_LEVEL"
+    if [[ "$LOG_SHOW_LEVEL" == "true" ]]; then
+      LOGGER="$LOGGER --show-level"
+    fi
+    if [[ "$LOG_SHOW_ORIGIN" == "true" ]]; then
+      LOGGER="$LOGGER --show-log-origin"
+    fi
+  else
+    LOGGER=$(cat <<EOF
   "logger": {
     "log_path": "$LOG_PATH",
     "level": "$LOG_LEVEL",
@@ -95,7 +190,34 @@ if [[ -v LOGGING ]]; then
     "show_log_origin": $LOG_SHOW_ORIGIN
   },
 EOF
-  )
+    )
+  fi
+fi
+
+if [[ -v RESTORE ]]; then
+  if [ ! -d "$RESTORE" ]; then
+    echo "Error: can't access snapshot directory $RESTORE"
+    exit 1
+  fi
+  rm -f $FC_SOCKET
+  echo "Running: firecracker --boot-timer --api-sock $FC_SOCKET $LOGGER"
+  firecracker --boot-timer --api-sock $FC_SOCKET $LOGGER &
+  ret=$(curl --write-out '%{http_code}' \
+             --silent \
+             --output /dev/null \
+             --unix-socket $FC_SOCKET \
+             -X PUT 'http://localhost/snapshot/load' \
+             -H  'Accept: application/json' \
+             -H  'Content-Type: application/json' \
+             -d "{ \"snapshot_path\": \"$RESTORE/snapshot_file\", \
+                   \"mem_backend\": { \
+                     \"backend_path\": \"$RESTORE/mem_file\", \
+                     \"backend_type\": \"File\" \
+                   }, \
+                   \"enable_diff_snapshots\": true, \
+                   \"resume_vm\": true }")
+  check_http_response $ret  "204" "Restore"
+  exit 0
 fi
 
 if [[ -v METRICS ]]; then
