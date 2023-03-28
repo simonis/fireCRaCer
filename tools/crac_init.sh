7764f5f851c1c88f4ceb555027b5b7eac8cd40e8
Volker Simonis <volker.simonis@gmail.com>
2023-03-28 20:15:44 +0200

    Changes before FOSDEM talk

--- tools/crac_init.sh
+++ tools/crac_init.sh
@@ -6,6 +6,10 @@
 # Use FC_JAVA_OPTIONS to pass java command line options from host to guest via kernel
 # boot parameters (we can't always use _JAVA_OPTIONS here because of JDK-8256844).
 
+# Define CONSOLE_LOG_PATTERN to change the log format. The default value is:
+# "%clr(%d{${LOG_DATEFORMAT_PATTERN:-yyyy-MM-dd HH:mm:ss.SSS}}){faint} %clr(${LOG_LEVEL_PATTERN:-%5p}) %clr(${PID:- }){magenta} %clr(---){faint} %clr([%15.15t]){faint} %clr(%-40.40logger{39}){cyan} %clr(:){faint} %m%n${LOG_EXCEPTION_CONVERSION_WORD:-%wEx}"
+export CONSOLE_LOG_PATTERN="%clr(${LOG_LEVEL_PATTERN:-%5p}) %clr(%-40.40logger{39}){cyan} %clr(:){faint} %m%n${LOG_EXCEPTION_CONVERSION_WORD:-%wEx}"
+
 echo "crac_init.sh: Starting petclinic" > /dev/kmsg
 
 echo_and_exec() { echo "  $@" > /dev/kmsg; "$@" ; }
